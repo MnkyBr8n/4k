dfadc6ab69bcb3b0d1f31cf43baef121a6f547b8
MnkyBr8n <yxyel315@outlook.com>
2026-02-09 16:10:05 -0800

    Update characters (0 total)

--- data/characters.js
+++ data/characters.js
@@ -1,15 +1,2 @@
 // data/characters.js
-export const CHARACTERS = {
-  "4K": {
-    "id": "char_1770407519753_b8kg0gd",
-    "template": "classic",
-    "name": "4K",
-    "category": "ocs",
-    "portrait": "oc/4k/portrait.png",
-    "link": "",
-    "website": "",
-    "dateOfBirth": "2011-11-27",
-    "placeOfBirth": "Springfield,MA",
-    "aboutQuick": "She is a failed(ish) science experiment."
-  }
-};
+export const CHARACTERS = {};
